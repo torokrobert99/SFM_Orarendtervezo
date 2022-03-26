8cd380d76667a6e6db89cbbc71a7015305ecb5af
torokrobert99 <torok.robert1999@gmail.com>
2022-03-26 18:11:09 +0100

    delete a line

--- src/main/java/hu/unideb/inf/FXMLOrarendSceneController.java
+++ src/main/java/hu/unideb/inf/FXMLOrarendSceneController.java
@@ -15,7 +15,6 @@ public class FXMLOrarendSceneController {
 
     @FXML
     void handledLoginButtonPushed(ActionEvent event) {
-        System.out.println(passwordField.getText());
         /* adok pár sor kommentet a teszt kedvéért */
         /* így ni nagyon szép lesz*/
 

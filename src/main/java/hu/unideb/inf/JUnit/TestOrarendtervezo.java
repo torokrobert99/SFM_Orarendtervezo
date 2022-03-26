b00c2a5a5284e7657fb0eefc63c6f48e5984aa2a
VictorBuscsei <victor.buscsei@gmail.com>
2022-03-26 18:08:05 +0100

    JUnit alap
    Ezek még egyenlőre hibás beállítások.

---
+++ src/main/java/hu/unideb/inf/JUnit/TestOrarendtervezo.java
@@ -0,0 +1,14 @@
+package hu.unideb.inf.JUnit;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestOrarendtervezo{
+    
+    @Test 
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}

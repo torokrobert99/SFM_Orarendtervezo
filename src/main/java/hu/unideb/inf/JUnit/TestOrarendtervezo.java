7ab7dc174acf9aec49204634f4bf4e7f58659394
VictorBuscsei <victor.buscsei@gmail.com>
2022-03-27 12:34:17 +0200

    JUnit fix
    .idea -ban sok helyen átírta a jdk 11-ről jdk 1.8-ra, meg a .idea misc.xml-ben jdk 16-ról 17-re.
    De így mostmár működik a JUnit 5:)

--- src/main/java/hu/unideb/inf/JUnit/TestOrarendtervezo.java
+++ src/main/java/hu/unideb/inf/JUnit/TestOrarendtervezo.java
@@ -2,7 +2,7 @@ package hu.unideb.inf.JUnit;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class TestOrarendtervezo{

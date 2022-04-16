0a3dda0693cfd6feb89d1c19cc94cb275e4ed247
czegeny.patrik <czegeny.patrik98@gmail.com>
2022-04-16 16:32:12 +0200

    Login felület funkcionalitásának lekódolása.

--- src/main/java/hu/unideb/inf/FXMLOrarendSceneController.java
+++ src/main/java/hu/unideb/inf/FXMLOrarendSceneController.java
@@ -1,19 +1,34 @@
 package hu.unideb.inf;
 
+import hu.unideb.inf.model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
-public class FXMLOrarendSceneController
-{
+public class FXMLOrarendSceneController {
+    private Model model;
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    JpaPersonDAO personDAO = new JpaPersonDAO();
+    JpaTeacherDAO teacherDAO = new JpaTeacherDAO();
+    JpaStudentDAO studentDAO = new JpaStudentDAO();
+    MainApp m = new MainApp();
+
+    @FXML
+    private Label errorMessage;
 
     @FXML
     private PasswordField passwordField;
@@ -24,25 +39,74 @@ public class FXMLOrarendSceneController
     @FXML
     private Button registerButton;
 
+    /**
+     * Ez a belépő gomb, ami továbbit a Menühöz ha a feltételek megfelelnek.
+     */
     @FXML
-    void handledLoginButtonPushed(ActionEvent event)
-    {
-        System.out.println("Hello world");
+    void handledLoginButtonPushed(ActionEvent event) throws IOException {
+        boolean checkFields = areTextFieldsEmpty();
+
+        if (checkFields) {
+            errorMessage.setText("Hiba! A fenti mezők kitöltése kötelező!");
+        } else {
+            loginAttempt();
+        }
 
     }
 
+    /**
+     * Ez a gonb kódja ami átvisz a regisztrációhoz.
+     */
     @FXML
-    void handledRegisterButtonPushed(ActionEvent event) throws IOException
-    {
-       MainApp m = new MainApp();
-       m.changeScene("/fxml/FXMLRegisterScene.fxml");
+    void handledRegisterButtonPushed(ActionEvent event) throws IOException {
+        MainApp m = new MainApp();
+        m.changeScene("/fxml/FXMLRegisterScene.fxml");
 
     }
 
-    boolean checkCredentials()
-    {
-        //TODO
-        return true;
+    /**
+     * Ez a metódus megnézi hogy üressek-e a belépési mezők.
+     */
+
+    public boolean areTextFieldsEmpty() {
+        return passwordField.getText().isEmpty() || usernameField.getText().isEmpty();
     }
 
+    /**
+     * Ez a metódus ellenőrzi a megadott bejelentkezési adatokat, és vissza tér a megadott felhasználóval.
+     */
+    public String loginAttempt() throws IOException {
+        MainApp m = new MainApp();
+        boolean validUser = false;
+        String compareUsername = usernameField.getText();
+        String comparePassword = passwordField.getText();
+        List<Teacher> listOfTeachersToCompare = teacherDAO.getTeachers();
+        List<Student> listOfStudentsToCompare = studentDAO.getStudents();
+        for (Student student : listOfStudentsToCompare) {
+            if (student.getUsername().equals(compareUsername)) {
+                validUser = true;
+                if (student.getPassword().equals(comparePassword)) {
+                    compareUsername = student.getUsername();
+                    m.changeScene("/fxml/FXMLMenuSceneController.fxml");
+                } else {
+                    errorMessage.setText("Hibás jelszó!");
+                }
+            }
+        }
+        for (Teacher teacher : listOfTeachersToCompare) {
+            if (teacher.getUsername().equals(compareUsername)) {
+                validUser = true;
+                if (teacher.getPassword().equals(comparePassword)) {
+                    compareUsername = teacher.getUsername();
+                    m.changeScene("/fxml/FXMLMenuSceneController.fxml");
+                } else {
+                    errorMessage.setText("Hibás jelszó!");
+                }
+            }
+        }
+        if (!validUser) {
+            errorMessage.setText("A megadott felhasználónév/jelszó páros helytelen!");
+        }
+        return compareUsername;
+    }
 }

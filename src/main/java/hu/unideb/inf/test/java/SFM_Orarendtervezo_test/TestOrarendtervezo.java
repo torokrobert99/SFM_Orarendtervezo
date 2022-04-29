f470b8d1a8ab38c5ad17aa47767401d3d4fc6a8d
VictorBuscsei <32177002+VictorBuscsei@users.noreply.github.com>
2022-04-29 21:26:55 +0200

    Pom.xml bővítése, test katalógus rendberakása
    Pom.xml bővítve Mockito-val, Hamcrest-el és Jacoco-val.
    Emiatt érdemes a <mvn dependency:purge-local-repository> command-ot lefuttatni, hogy a függőségek letöltődjenek (VS-Code), vagy a maven dependency manager (IntelliJ IDEA) egy kis ablakot felhoz amelyre kattintva letöltődnek automatikusan a függőségek.
    Test katalógus rendberakva.

---
+++ src/main/java/hu/unideb/inf/test/java/SFM_Orarendtervezo_test/TestOrarendtervezo.java
@@ -0,0 +1,31 @@
+package hu.unideb.inf.test.java.SFM_Orarendtervezo_test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.mockito.BDDMockito.given;     //Itt az org.mockito-ra aláhúzza, később javítom.
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.MockitoAnnotations;
+
+
+public class TestOrarendtervezo{
+    //próba Junit teszt
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}

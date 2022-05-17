e1e11b390583f0e58d6ea7dcd2c88e36b4c05145
Xenole <kissdanielerik0112@gmail.com>
2022-05-17 20:37:11 +0200

    Órarendtervező tesztelése

--- src/main/java/hu/unideb/inf/JUnit/TestOrarendtervezo.java
+++ src/main/java/hu/unideb/inf/JUnit/TestOrarendtervezo.java
@@ -2,14 +2,16 @@ package hu.unideb.inf.JUnit;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class TestOrarendtervezo{
     //próba Junit teszt
+
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void test(){
+        assertEquals(1, 1);
     }
 
 }
